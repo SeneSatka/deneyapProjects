b100b1c9efe689c636b6467ed43a66a10ba95090
SeneSatka <senesatka@gmail.com>
2023-10-21 23:32:45 +0300

    piezo control added and fixed bugs

--- MalzemeBilimiveNanoTeknoloji/KartCode/src/main.cpp
+++ MalzemeBilimiveNanoTeknoloji/KartCode/src/main.cpp
@@ -1,3 +1,8 @@
+/*
+###### Enes AKTAŞ #########
+###### 21.10.2023 #########
+###### PlatformIO Linux ###
+*/
 #include <Arduino.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
@@ -5,18 +10,28 @@
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
 #include <iostream>
-const char *ssid = "FiberHGW_ZTAT49_2.4GHz";
-const char *password = "33khNYhCUeDf";
-const char *server = "http://192.168.1.116:3000/deneyapkartRequest";
+const char *ssid = "deneyapGebze";
+const char *password = "deneyap11";
+const char *ssid1 = "DENEYAP 2";
+const char *password1 = "deneyap22";
+const char *ssid2 = "FiberHGW_ZTAT49_2.4GHz";
+const char *password2 = "33khNYhCUeDf";
+const char *server = "https://mlzmblmvnntknljprjsrv.senesatka.repl.co/deneyapkartRequest";
+HTTPClient http;
+
 DynamicJsonDocument Leds(1024);
 const int ledPins[6] = {D2, D3, D4, D5, D6, D7};
+int ledStates[6] = {LOW, LOW, LOW, LOW, LOW, LOW};
+const int piezoPins[3] = {A0, A1, A2};
 void connectWifi();
 void getRequest(HTTPClient &client, const char *uri);
+void piezoLedsControl();
 void setup()
 {
   pinMode(LEDR, OUTPUT);
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
+  pinMode(A0, INPUT);
   for (int i = 0; i < 6; i++)
   {
     String ledObject = "{\"id\":" + String(i) + ",isOpen:false}";
@@ -26,42 +41,111 @@ void setup()
     digitalWrite(ledPins[i], LOW);
     Leds[String(i)] = object;
   }
+  for (int i = 0; i < 3; i++)
+  {
+    pinMode(piezoPins[i], INPUT);
+  }
   Serial.begin(115200);
   connectWifi();
 }
 
 void loop()
 {
-  while (WiFi.status() != WL_CONNECTED)
+  piezoLedsControl();
+  if (WiFi.status() == WL_CONNECTED)
+    getRequest(http, server);
+}
+void piezoLedsControl()
+{
+  int piezo1State = map(analogRead(A0), 8191, 0, 100, 0);
+  int piezo2State = map(analogRead(A1), 8191, 0, 100, 0);
+  int piezo3State = map(analogRead(A2), 8191, 0, 100, 0);
+  int piezoLimit[3] = {50, 50, 50};
+  Serial.print("1: ");
+  Serial.println(piezo1State);
+  Serial.print("2: ");
+  Serial.println(piezo2State);
+  Serial.print("3: ");
+  Serial.println(piezo3State);
+  if (piezo1State > piezoLimit[0])
+  {
+    ledStates[0] = HIGH;
+    ledStates[3] = HIGH;
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[3], HIGH);
+  }
+  if (ledStates[0] != HIGH)
+  {
+    ledStates[0] = LOW;
+    digitalWrite(ledPins[0], LOW);
+  }
+  if (ledStates[3] != HIGH)
+  {
+    ledStates[3] = LOW;
+    digitalWrite(ledPins[3], LOW);
+  }
+  if (piezo2State > piezoLimit[1])
+  {
+    ledStates[1] = HIGH;
+    ledStates[4] = HIGH;
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[4], HIGH);
+  }
+  if (ledStates[1] != HIGH)
   {
 
-    digitalWrite(LEDR, HIGH);
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDB, LOW);
+    ledStates[1] = LOW;
+    digitalWrite(ledPins[1], LOW);
   }
-  HTTPClient http;
-  delay(1000);
-  getRequest(http, server);
+  if (ledStates[4] != HIGH)
+  {
+    ledStates[4] = LOW;
+    digitalWrite(ledPins[4], LOW);
+  }
+  if (piezo3State > piezoLimit[2])
+  {
+    ledStates[2] = HIGH;
+    ledStates[5] = HIGH;
+    digitalWrite(ledPins[2], HIGH);
+    digitalWrite(ledPins[5], HIGH);
+  }
+  if (ledStates[2] != HIGH)
+  {
+    ledStates[2] = LOW;
+    digitalWrite(ledPins[2], LOW);
+  }
+  if (ledStates[5] != HIGH)
+  {
+    ledStates[5] = LOW;
+    digitalWrite(ledPins[5], LOW);
+  }
 }
-
 void connectWifi()
 {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
+  byte ssidNumList = WiFi.scanNetworks();
+  // print the network number and name for each network found:
+  for (int currentSsid = 0; currentSsid < ssidNumList; currentSsid++)
   {
-    digitalWrite(LEDR, HIGH);
-    Serial.print("Wi-Fi bağlantısı kuruluyor");
-    delay(250);
-    Serial.print(".");
-    delay(250);
-    Serial.print(".");
-    delay(250);
-    Serial.print(".");
-    delay(250);
+    if (WiFi.SSID(currentSsid) == ssid)
+    {
+      WiFi.begin(ssid, password);
+      break;
+    }
+    else if (WiFi.SSID(currentSsid) == ssid1)
+    {
+      WiFi.begin(ssid1, password1);
+      break;
+    }
+    else if (WiFi.SSID(currentSsid) == ssid2)
+    {
+      WiFi.begin(ssid2, password2);
+      break;
+    }
+    else
+      break;
   }
   digitalWrite(LEDR, LOW);
   digitalWrite(LEDG, HIGH);
-  Serial.println("Wi-Fi bağlantısı kuruldu.");
 }
 void getRequest(HTTPClient &client, const char *uri)
 {
@@ -79,29 +163,21 @@ void getRequest(HTTPClient &client, const char *uri)
     DynamicJsonDocument jsonObject(1024);
 
     deserializeJson(jsonObject, res);
-    // for (int i = 0; i < 6; i++)
-    // {
-    //   Serial.println(String(ledPins[i]));
-    //   const char *id = jsonObject[String(i)]["id"];
-    //   Serial.println(id);
-    // }
+
     for (JsonPair keyValue : jsonObject.as<JsonObject>())
     {
       JsonObject ledObject = keyValue.value().as<JsonObject>();
       int id = ledObject["id"];
       bool isOpen = ledObject["isOpen"];
-      // Serial.print("LED ID: ");
-      // Serial.println(ledPins[id]);
-      // Serial.print("Is Open: ");
       if (isOpen)
       {
-        digitalWrite(ledPins[id], HIGH);
+        ledStates[id] = HIGH;
+        digitalWrite(ledPins[id], ledStates[id]);
       }
       else
       {
-        digitalWrite(ledPins[id], LOW);
+        ledStates[id] = LOW;
       }
-      Serial.println(isOpen ? "true" : "false");
     }
   }
   else
